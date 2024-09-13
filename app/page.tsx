8ea9f7582935d45103ada81d41f87963e6f976f0
Todd Heckeler <todd@consumedesign.com>
2024-09-13 11:37:19 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,173 @@
-"use client"
-import { Formik } from 'formik';
+"use client";
+// pages/form.tsx
 
-const Basic = () => (
-  <div>
-    <h1>Anywhere in your app!</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-            required
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+import {
+	Box,
+	Button,
+	Checkbox,
+	FormControlLabel,
+	FormGroup,
+	TextField,
+	Typography,
+} from "@mui/material";
+import { ErrorMessage, Form, Formik } from "formik";
+import React from "react";
 
-export default Basic;
+interface FormValues {
+	name: string;
+	email: string;
+	password: string;
+	terms: boolean;
+}
+
+const MyForm: React.FC = () => {
+	// Initial form values
+	const initialValues: FormValues = {
+		name: "",
+		email: "",
+		password: "",
+		terms: false,
+	};
+
+	// Form submission handler
+	const handleSubmit = (
+		values: FormValues,
+		{ setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
+	) => {
+		console.log("Form submitted with values:", values);
+		setSubmitting(false);
+	};
+
+	// Simple validation function
+	const validate = (values: FormValues) => {
+		const errors: Partial<FormValues> = {};
+
+		if (!values.name) {
+			errors.name = "Name is required";
+		}
+		if (!values.email) {
+			errors.email = "Email is required";
+		} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+			errors.email = "Invalid email address";
+		}
+		if (!values.password) {
+			errors.password = "Password is required";
+		} else if (values.password.length < 8) {
+			errors.password = "Password must be at least 8 characters";
+		}
+		if (!values.terms) {
+			errors.terms = "You must accept the terms and conditions";
+		}
+
+		return errors;
+	};
+
+	return (
+		<Box sx={{ maxWidth: 400, margin: "auto", padding: 2 }}>
+			<Typography variant="h4" align="center" gutterBottom>
+				Formik Form Example
+			</Typography>
+
+			<Formik
+				initialValues={initialValues}
+				validate={validate}
+				onSubmit={handleSubmit}
+			>
+				{({ isSubmitting, handleChange, values, handleBlur }) => (
+					<Form>
+						{/* Name Field */}
+						<FormGroup sx={{ marginBottom: 2 }}>
+							<TextField
+								id="name"
+								name="name"
+								label="Name"
+								variant="outlined"
+								fullWidth
+								onChange={handleChange}
+								onBlur={handleBlur}
+								value={values.name}
+							/>
+							<ErrorMessage
+								name="name"
+								component="div"
+								style={{ color: "red", marginTop: "5px" }}
+							/>
+						</FormGroup>
+
+						{/* Email Field */}
+						<FormGroup sx={{ marginBottom: 2 }}>
+							<TextField
+								id="email"
+								name="email"
+								label="Email"
+								variant="outlined"
+								fullWidth
+								onChange={handleChange}
+								onBlur={handleBlur}
+								value={values.email}
+							/>
+							<ErrorMessage
+								name="email"
+								component="div"
+								style={{ color: "red", marginTop: "5px" }}
+							/>
+						</FormGroup>
+
+						{/* Password Field */}
+						<FormGroup sx={{ marginBottom: 2 }}>
+							<TextField
+								id="password"
+								name="password"
+								label="Password"
+								type="password"
+								variant="outlined"
+								fullWidth
+								onChange={handleChange}
+								onBlur={handleBlur}
+								value={values.password}
+							/>
+							<ErrorMessage
+								name="password"
+								component="div"
+								style={{ color: "red", marginTop: "5px" }}
+							/>
+						</FormGroup>
+
+						{/* Terms Checkbox */}
+						<FormGroup sx={{ marginBottom: 2 }}>
+							<FormControlLabel
+								control={
+									<Checkbox
+										id="terms"
+										name="terms"
+										checked={values.terms}
+										onChange={handleChange}
+									/>
+								}
+								label="I accept the terms and conditions"
+							/>
+							<ErrorMessage
+								name="terms"
+								component="div"
+								style={{ color: "red", marginTop: "5px" }}
+							/>
+						</FormGroup>
+
+						{/* Submit Button */}
+						<Button
+							type="submit"
+							variant="contained"
+							color="primary"
+							fullWidth
+							disabled={isSubmitting}
+						>
+							{isSubmitting ? "Submitting..." : "Submit"}
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Box>
+	);
+};
+
+export default MyForm;
